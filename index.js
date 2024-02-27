52347caaf4d836e9292cd378f83e8bd6819d7443
Nicolas de Garrigues <ndegarrigues@gmail.com>
2024-02-27 16:59:14 +0100

    bug: add cors

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ import express from 'express'
 import dotenv from 'dotenv'
 import router from './router.js'
 import characters from './routes/CharacterRoutes.js'
+import cors from 'cors'
 
 const app = express()
 dotenv.config()
 
+app.use(cors())
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
 app.use(router);
